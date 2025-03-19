6f8b64014154d2f9e84a4eb8db08f84eb19c211d
Magno_V_Gomes <magnovgomes74@gmail.com>
2025-03-19 17:08:46 +0000

    Finalizando o nível novato do jogo batalha naval

--- nivelNovato.c
+++ nivelNovato.c
@@ -10,13 +10,12 @@ int main() {
    
     int tabuleiro[10][10]; // Tabuleiro 10x10
     int i, j; // Índices para linhas e colunas
-    int linhaV = 2, colunaH = 7; // Linha e coluna para posicionamento dos navios
-    char colunas[] = "ABCDEFGHIJ"; // Letras para colunas
 
-    // Inicializa o tabuleiro com água (0)   
 
-    printf("**TABULEIRO BATALHA NAVAL**\n\n");
+    printf("***TABULEIRO DE BATALHA NAVAL***\n\n");
 
+
+    // Inicializando o tabuleiro com água (0)   
     for (i = 0; i < 10; i++) {
         for (j = 0; j < 10; j++) {
             tabuleiro[i][j] = 0;
@@ -24,40 +23,56 @@ int main() {
     }
 
 
-    // Posicionando um navio verticalmente na linha 3, colunas 3 a 5.
-
-    for (j = 2; j < 5; j++) {
-        tabuleiro[linhaV][j] = 3;
-    }
-
-    // Posicionando um navio horizontalmente na coluna H (linha 5 a 7).
-
-    for (i = 4; i < 7; i++) {
-        tabuleiro[i][colunaH] = 3;
-    }
-
-    // Sugestão: Utilize printf para exibir as coordenadas de cada parte dos navios.
-    // Exibir tabuleiro com coordenadas
-
-     printf("   ");
-
-     for (j = 0; j < 8; j++) { // Ajustado para colunas de A a H
-         printf("%c ", colunas[j]);
-     }
-
-     printf("\n");   
-
-     for (i = 0; i < 10; i++) {
+    // Exibe o tabuleiro com água (0)
+    printf("**Tabuleiro com água (0)**\n\n");
+    printf("   A B C D E F G H I J\n");
+    for (i = 0; i < 10; i++) {
         printf("%2d ", i + 1);
-        for (j = 0; j < 8; j++) { // Ajustado para colunas de A a H
+        for (j = 0; j < 10; j++) {
             printf("%d ", tabuleiro[i][j]);
         }
         printf("\n");
     }
-         
+    printf("\n");
 
-    printf("\n\n");
 
+    // Posicionando um navio verticalmente na Coluna C (índices 3 a 5)
+    for (i = 2; i < 5; i++) {
+        tabuleiro[i][2] = 3;
+    }
+
+
+    // Exibe o tabuleiro com o navio verticalmente
+    printf("**Tabuleiro com navio vertical**\n\n");
+    printf("   A B C D E F G H I J\n");
+    for (i = 0; i < 10; i++) {
+        printf("%2d ", i + 1);
+        for (j = 0; j < 10; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+
+    // Posicionando um navio horizontalmente na Linha 5 (colunas H a J)
+    for (j = 7; j < 10; j++) {
+        tabuleiro[4][j] = 3;
+    }
+
+
+    // Exibe o tabuleiro com o navio horizontalmente
+    printf("**Tabuleiro com navio horizontal**\n\n");
+    printf("   A B C D E F G H I J\n");
+    for (i = 0; i < 10; i++) {
+        printf("%2d ", i + 1);
+        for (j = 0; j < 10; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n\n\n");
 
     return 0;
 }
