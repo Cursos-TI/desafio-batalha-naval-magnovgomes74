251a39f12984155b566248650e9b0d695164749b
Magno_V_Gomes <magnovgomes74@gmail.com>
2025-03-19 18:05:29 +0000

    Finalizando o nivel Aventureiro do jogo batalha naval

--- teste.c
+++ teste.c
@@ -5,15 +5,10 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-
     int tabuleiro[10][10]; // Tabuleiro 10x10
     int i, j; // Índices para linhas e colunas
 
-
     printf("***TABULEIRO DE BATALHA NAVAL***\n\n");
-
 
     // Inicializando o tabuleiro com água (0)   
     for (i = 0; i < 10; i++) {
@@ -21,7 +16,6 @@ int main() {
             tabuleiro[i][j] = 0;
         }
     }
-
 
     // Exibe o tabuleiro com água (0)
     printf("**Tabuleiro com água (0)**\n\n");
@@ -35,18 +29,15 @@ int main() {
     }
     printf("\n");
 
-
     // Posicionando um navio verticalmente na Coluna C (índices 3 a 5)
     for (i = 2; i < 5; i++) {
         tabuleiro[i][2] = 3;
     }
 
-
     // Posicionando um navio horizontalmente na Linha 5 (colunas H a J)
     for (j = 7; j < 10; j++) {
         tabuleiro[4][j] = 3;
     }
-
 
     // Exibe o tabuleiro com dois navios posicionados na vertical e horizontal;
     printf("**Tabuleiro com dois navios na vertical e horizontal**\n\n");
@@ -59,7 +50,6 @@ int main() {
         printf("\n");
     }
     printf("\n");
-
 
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
@@ -80,7 +70,6 @@ int main() {
 
     printf("**Tabuleiro apenas com a diagonal principal preenchida**\n\n");
     printf("   A B C D E F G H I J\n");
-
     for (i = 0; i < 10; i++) {
         printf("%2d ", i + 1);
         for (j = 0; j < 10; j++) {
@@ -91,63 +80,14 @@ int main() {
     printf("\n");
 
 
-        // Posicionando um navio na diagonal oposta de todo tabuleiro.
 
-    for (i = 0; i < 10; i++) {
-        for (j = 0; j < 10; j++) {
-            tabuleiro[i][j] = 0;
-        }
-    }
-    for (i = 0; i < 10; i++) {
-        tabuleiro[i][9-i] = 3;
-    }
+    // Exibe o tabuleiro com todos os navios posicionados, incluindo na diagonal e na horizontal/vertical.
 
-    // Exibe o tabuleiro apenas com a diagonal oposta preenchida
-
-    printf("**Tabuleiro apenas com a diagonal oposta preenchida**\n\n");
-    printf("   A B C D E F G H I J\n");
-
-    for (i = 0; i < 10; i++) {
-        printf("%2d ", i + 1);
-        for (j = 0; j < 10; j++) {
-            printf("%d ", tabuleiro[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-
-
-    // Posicinando um navio na diagonal principal com três indices(2,C), (3,D), (4,E).
-    // Posicionando um navio na diagonal oposta com três indices(1,H), (2,G), (3,F).
-
-    for (i = 0; i < 10; i++) {
-        for (j = 0; j < 10; j++) {
-            tabuleiro[i][j] = 0;
-        }
-    }
-    tabuleiro[3][2] = 3;
-    tabuleiro[4][3] = 3;
-    tabuleiro[5][4] = 3;
-    tabuleiro[2][7] = 3;
-    tabuleiro[3][6] = 3;
-    tabuleiro[4][5] = 3;
     
-
-
-    // Exibe o tabuleiro com as duas diagonais preenchidas
-
-    printf("**Tabuleiro com as duas diagonais preenchidas**\n\n");
-    printf("   A B C D E F G H I J\n");
-
-    for (i = 0; i < 10; i++) {
-        printf("%2d ", i + 1);
-        for (j = 0; j < 10; j++) {
-            printf("%d ", tabuleiro[i][j]);
-        }
-        printf("\n");
-    }
-
-    printf("\n\n\n");
+    
+    
+    
+    printf("\n\n");
 
     return 0;
 }
